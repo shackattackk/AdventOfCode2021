274896654c3cdc3c1f772e3339c6b3deaf071fec
shackattackk <gaby_yusay2001@yahoo.com>
2021-12-03 17:10:24 +0800

    solved day 2

---
+++ Day_2/day2_b.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//	File Opening
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("File not found")
+		return
+	}
+	defer file.Close()
+
+	//  Read Input
+
+	sc := bufio.NewScanner(file)
+	var values []string
+	for sc.Scan() {
+		values = append(values, sc.Text())
+	}
+
+	//solve for the final position
+	horizontal, depth, aim := 0, 0, 0
+
+	for _, val := range values {
+		res := strings.Split(val, " ")
+		if res[0] == "forward" {
+			if i, err := strconv.Atoi(res[1]); err == nil {
+				horizontal += i
+				depth += aim * i
+			}
+		} else if res[0] == "up" {
+			if i, err := strconv.Atoi(res[1]); err == nil {
+				aim -= i
+			}
+		} else if res[0] == "down" {
+			if i, err := strconv.Atoi(res[1]); err == nil {
+				aim += i
+			}
+		}
+
+	}
+	fmt.Println(horizontal * depth)
+
+}
